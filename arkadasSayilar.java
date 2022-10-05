4e95cdbab41a92f5ec665df4342611621dbac7d2
MuhammedTafli <koska_33@hotmail.com>
2022-10-05 11:34:31 +0300

    Arkadaş sayıları bulma projesi tamamlandı.

---
+++ arkadasSayilar.java
@@ -0,0 +1,28 @@
+public class arkadasSayilar {
+    public static void main(String[] args) {
+        // Kendileri hariç pozitif tam bölenlerinin toplamı birbirine eşit olan sayılara
+        // arkadaş sayı denir.
+
+        int sayi1 = 453;
+        int sayi2 = 432;
+        int toplam1 = 0;
+        int toplam2 = 0;
+
+        for (int i = 1; i < sayi1; i++) {
+            if (sayi1 % i == 0) {
+                toplam1 = toplam1 + i;
+            }
+        }
+        for (int i = 1; i < sayi2; i++) {
+            if (sayi2 % i == 0) {
+                toplam2 = toplam2 + i;
+            }
+        }
+        if (sayi1 == toplam2 && sayi2 == toplam1) {
+            System.out.println("Bu iki sayı arkadaştır.");
+        } else {
+            System.out.println("Bu iki sayı arkadaş değildir.");
+
+        }
+    }
+}
